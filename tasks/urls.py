3e2b24a01409f2982526e9f4f54968d1e1842760
tn <tn59249@yahoo.com>
2025-06-27 17:10:58 +0600

    dynamic_urls bug fixes

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from tasks.views import show_task, show_specific_task
 
 urlpatterns = [
-    path("show-task/", show_task)
+    path("show-task/", show_task),
     path("show-task/<int:id>", show_specific_task),
 ]
